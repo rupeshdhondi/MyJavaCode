7c7e53791de4a6a72b70b1f804394357983d8a99
rupeshdhondi <rupeshdhondi@users.noreply.github.com>
2018-03-11 16:20:18 +0530

    updated line of code

--- Hello.java
+++ Hello.java
@@ -2,9 +2,11 @@ public class Hello
 {
 	public static void main(String[] args)
 	{
+		List<String> list = new ArrayList<>();
 		System.out.println("Hello");
 System.out.println("------------");
 System.out.println("hi");
+		
 	}
 	
 }
